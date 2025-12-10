62ac5862f9a57677e9544b48923f663da55ae849
Rich Snapp <rich.snapp@gmail.com>
2025-12-09 21:21:04 -0700

    update ./up so that it's smart about attaching to old ssh-agent

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# remove any stale sockets from a previous run
+rm -f /tmp/ssh-agent-dev
+
 socat UNIX-LISTEN:/tmp/ssh-agent-dev,fork,user=dev,group=dev,mode=600 \
   UNIX-CONNECT:/ssh-agent &
 
